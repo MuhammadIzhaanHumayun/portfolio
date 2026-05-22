808b055e6449534a727fb364a84711f67315b910
Muhammad Izhaan Humayun <149602934+MuhammadIzhaanHumayun@users.noreply.github.com>
2026-05-22 19:33:49 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
     plugins: [react(), tailwindcss()],
-    base: "/portfolio/",
+    base: process.env.NODE_ENV === "production" ? "./" : "/portfolio/",
     optimizeDeps: {
       esbuildOptions: {
         loader: {
